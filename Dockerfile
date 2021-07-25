3d10263a657c921d1d376f909a69f59ddb291f68
Ashish Sapra <a.ash.bus@gmail.com>
2021-07-25 22:11:40 +0530

    file for test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM continuumio/anaconda3
-WORKDIR /usr/app/
-COPY . .
+FROM continuumio/anaconda3:4.4.0
+COPY . /usr/app/
 EXPOSE 5000
-RUN pip install -r requirements.txt 
+WORKDIR /usr/app/
+RUN pip install -r requirements.txt
 CMD python flask_api.py
--- flask_api.py
+++ flask_api.py
@@ -1,31 +1,28 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 22 00:06:11 2021
+Created on Fri May 15 12:50:04 2020
 
-@author: ashishsapra
+@author: krish.naik
 """
 
 from flask import Flask, request
-import pandas as pd
 import numpy as np
 import pickle
+import pandas as pd
 import flasgger
 from flasgger import Swagger
 
-app = Flask(__name__)
+app=Flask(__name__)
 Swagger(app)
 
-pickle_in = open('classifier.pkl','rb')
-classifier = pickle.load(pickle_in) 
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
     return "Welcome All"
 
-
-
-@app.route('/predict')
+@app.route('/predict',methods=["Get"])
 def predict_note_authentication():
     
     """Let's Authenticate the Banks Note 
@@ -53,36 +50,37 @@ def predict_note_authentication():
             description: The output values
         
     """
-    
-    variance = request.args.get('variance')
-    skewness = request.args.get('skewness')
-    curtosis = request.args.get('curtosis')
-    entropy = request.args.get('entropy')
-    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
-    return "The predicited values is " + str(prediction)
-
+    variance=request.args.get("variance")
+    skewness=request.args.get("skewness")
+    curtosis=request.args.get("curtosis")
+    entropy=request.args.get("entropy")
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return "Hello The answer is"+str(prediction)
 
 @app.route('/predict_file',methods=["POST"])
 def predict_note_file():
-    
-    """Let's Authenticate the Bank Notes
+    """Let's Authenticate the Banks Note 
     This is using docstrings for specifications.
     ---
     parameters:
-        - name: file
-          in: formData
-          type: file
-          required: true
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
     responses:
         200:
-            description: The output Value
+            description: The output values
+        
     """
-    
     df_test=pd.read_csv(request.files.get("file"))
     print(df_test.head())
     prediction=classifier.predict(df_test)
     
     return str(list(prediction))
 
-if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000)
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=8000)
+    
+    
