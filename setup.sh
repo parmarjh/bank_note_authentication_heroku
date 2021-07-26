69c3a4d1e437b4fc54629137b3cc33e5dad87b15
Ashish Sapra <a.ash.bus@gmail.com>
2021-07-26 15:15:54 +0530

    added email

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-mkdir -p ~/.streamlit/echo "[general]  email = \"me@gmail.com\""  > ~/.streamlit/credentials.tomlecho "[server]headless = trueport = $PORTenableCORS = true"  >> ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/echo "[general]  email = \"a.ash.bus@gmail.com\""  > ~/.streamlit/credentials.tomlecho "[server]headless = trueport = $PORTenableCORS = true"  >> ~/.streamlit/config.toml
