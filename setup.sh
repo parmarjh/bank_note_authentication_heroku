3a4f474841034c315891ccf0283f97d50411f816
Ashish Sapra <a.ash.bus@gmail.com>
2021-07-26 15:10:49 +0530

    test chnages

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-mkdir -p ~/.streamlit/echo "[general]"  > ~/.streamlit/credentials.tomlecho "email = \"my@email.com\""  >> ~/.streamlit/credentials.tomlecho "[server]"  > ~/.streamlit/config.toml echo "headless = true"  >> ~/.streamlit/config.tomlecho "port = $PORT"  >> ~/.streamlit/config.tomlecho "enableCORS = true"  >> ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/echo "[general]  email = \"me@gmail.com\""  > ~/.streamlit/credentials.tomlecho "[server]headless = trueport = $PORTenableCORS = true"  >> ~/.streamlit/config.toml
