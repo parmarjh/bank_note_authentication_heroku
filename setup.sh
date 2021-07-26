cff590433dd3f227300c0e4eeca1ef00a575f67e
Ashish Sapra <a.ash.bus@gmail.com>
2021-07-26 14:49:37 +0530

    changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-mkdir -p ~/.streamlit/echo "\[general]\n\email = \"your-email@domain.com\"\n\" > ~/.streamlit/credentials.tomlecho "\[server]\n\headless = true\n\enableCORS=false\n\port = $PORT\n\" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/echo "[general]"  > ~/.streamlit/credentials.tomlecho "email = \"my@email.com\""  >> ~/.streamlit/credentials.tomlecho "[server]"  > ~/.streamlit/config.toml echo "headless = true"  >> ~/.streamlit/config.tomlecho "port = $PORT"  >> ~/.streamlit/config.tomlecho "enableCORS = true"  >> ~/.streamlit/config.toml
